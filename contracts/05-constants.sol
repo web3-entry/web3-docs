6189365e77bf83ec350c20d9c97d212765e6fa6a
tiantingrui <976912095@qq.com>
2023-09-17 11:57:57 +0800

    solidity 中的常量

---
+++ contracts/05-constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+
+contract Constants {
+    // 常量用大写
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
+}
